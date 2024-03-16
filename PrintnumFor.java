2c80a58c8188c337405039948564397086247abb
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-03-16 09:13:43 +0530

    Add files via upload

---
+++ PrintnumFor.java
@@ -0,0 +1,11 @@
+class PrintnumFor
+  {
+   //a whole number greater than 1 whose only factors are 1 and itself
+   public static void main(String[]args)
+    {
+     for(int a=1;a<=10;a++)
+      {
+       System.out.println(a);
+      }
+    }
+  }
